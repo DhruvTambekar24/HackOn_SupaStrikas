4c1faf30e5a3e35b21bc8cc91fd1c6cca73ce50e
MohdShayan <sehbaqureshi@gmail.com>
2025-02-21 12:49:07 +0530

    new file:   Skin Cancer Cell Project/static/uploads/bf1e725d-5b10-488f-9611-184068e03b94.jpg 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/HomePage.jsx 	modified:   src/components/LandingPage.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import LandingPage from "./components/LandingPage"
 import './App.css'
 import SkinCancerDetection from './components/SkinCancer'
-
+import LandingPage from './components/LandingPage'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
 
   return (
     <div className='m-0 p-0'>
-      {/* <LandingPage/> */}
-    <SkinCancerDetection/>
+<Routes>
+  <Route path="/" element={<LandingPage />} />
+  <Route path="/skin" element={<SkinCancerDetection />} />
+</Routes>
     </div>
   )
 }
